5ca97a8f87b28d883b26c4ee1d1ca6700857cb15
hoi <coghd28@naver.com>
2023-11-02 17:59:36 +0900

    Update <김채홍 : urls_todo.py>
    url수정

--- manda_project/manda_app/manda_urls/urls_todo.py
+++ manda_project/manda_app/manda_urls/urls_todo.py
@@ -4,9 +4,9 @@ from ..manda_views import views_todo
 
 urlpatterns = [
     # 다른 URL 매핑들...
-    path('todolist/view/', views_todo.todo_list_view, name='todo_list'),
-    path('todolist/wirte/', views_todo.create_todo_view, name='create_todo'),
-    path('todolist/detail/<int:id>/', views_todo.get_todo_detail_view, name='todo_detail'),
-    path('todolist/updqte/<int:id>/', views_todo.update_todo_view, name='update_todo'),
-    path('todolist/delete/<int:id>/', views_todo.delete_todo_view, name='delete_todo'),
+    path('view/', views_todo.todo_list_view, name='todo_list'),
+    path('wirte/', views_todo.create_todo_view, name='create_todo'),
+    path('detail/<int:id>/', views_todo.get_todo_detail_view, name='todo_detail'),
+    path('updqte/<int:id>/', views_todo.update_todo_view, name='update_todo'),
+    path('delete/<int:id>/', views_todo.delete_todo_view, name='delete_todo'),
 ]
