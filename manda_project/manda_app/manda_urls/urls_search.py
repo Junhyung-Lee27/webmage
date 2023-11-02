dbf0d2296c5422ad365f7a1b2f6c608a67dfa351
jhlee <makk7201@gmail.com>
2023-11-03 04:16:47 +0900

    Update <이준형 : 컴포넌트별 검색결과>

--- manda_project/manda_app/manda_urls/urls_search.py
+++ manda_project/manda_app/manda_urls/urls_search.py
@@ -3,4 +3,7 @@ from ..manda_views import views_search
 
 urlpatterns = [
     path('', views_search.search_view, name='search'),
+    path('mandasimple/', views_search.search_manda_simples, name='search_manda_simples'),
+    path('feeds/', views_search.search_feeds, name='search_feeds'),
+    path('users/', views_search.search_users, name='search_users'),
 ]
