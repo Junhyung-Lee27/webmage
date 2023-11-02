39e8667500d2399812eb99189cfd2d64f031ebf3
hoi <coghd28@naver.com>
2023-11-02 17:12:44 +0900

    Add <김채홍 : urls.py>
    todo url 추가

--- manda_project/manda_app/urls.py
+++ manda_project/manda_app/urls.py
@@ -9,6 +9,7 @@ from .manda_urls.urls_user import urlpatterns as manda_user_urls
 from .manda_urls.urls_write import urlpatterns as manda_write_urls
 from .manda_urls.urls_manda import urlpatterns as manda_manda_urls
 from .manda_urls.urls_feed import urlpatterns as manda_feed_urls
+from .manda_urls.urls_todo import urlpatterns as manda_todo_urls
 from .manda_urls.urls_chat import urlpatterns as manda_chat_urls
 from .manda_urls.urls_search import urlpatterns as manda_search_urls
 
@@ -19,6 +20,7 @@ urlpatterns = [
     path('write/', include(manda_write_urls)), #글 작성, 글 선택
     path('manda/', include(manda_manda_urls)), #만다라트
 	path('feed/', include(manda_feed_urls)), #피드
+    path('todo/', include(manda_todo_urls)), #할 일(todo)
     path('chat/', include(manda_chat_urls)), #채팅
     path('search/', include(manda_search_urls)), #검색(탐색)
     path('get_token/', views.get_csrf_token, name='get_token'), #토큰
