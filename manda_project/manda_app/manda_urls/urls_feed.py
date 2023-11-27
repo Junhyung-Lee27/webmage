93baee2d3a5aee639f30d526744648d28d8c6a88
jhlee <makk7201@gmail.com>
2023-11-27 17:17:44 +0900

    Delete <이준형 : timeline/>
    - recommend/ 와 같은 역할의 함수이기 때문에 삭제함

--- manda_project/manda_app/manda_urls/urls_feed.py
+++ manda_project/manda_app/manda_urls/urls_feed.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('<int:user_id>/', views_feed.return_feed, name='user_feed'),
     path('recommend/', views_feed.recommend_feeds, name='recommend_feed'),
     path('<int:user_id>/log/', views_feed.return_feed_log, name='user_feed_log'),
-    path('timeline/<int:user_id>/', views_feed.return_timeline, name='user_timeline'),
     path('write/', views_feed.write_feed, name='write_feed'),
     path('<int:feed_id>/', views_feed.edit_feed, name='edit_feed'), # Assuming PATCH method is handled in this view.
     path('<int:feed_id>/set_emoji/', views_feed.set_feed_emoji, name='set_feed_emoji'),
